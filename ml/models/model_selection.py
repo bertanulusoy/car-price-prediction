0c3ee43131fb1fb499dbea770172356162501e33
Bertan ULUSOY <bertan_ulusoy@yahoo.com>
2023-06-11 20:50:25 +0300

    Model Selection class added.

--- ml/models/model_selection.py
+++ ml/models/model_selection.py
@@ -5,14 +5,16 @@ from numpy import ndarray
 
 from pandas import DataFrame, Series, Index
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 
 
 @dataclass()
-class Model:
+class ModelSelection:
     data_frame: DataFrame
     target: str
+    test_size: float
+    alpha: int = None
 
     __X: ndarray = None
     __y: ndarray = None
@@ -22,26 +24,34 @@ class Model:
     __y_train = None
     __y_test = None
 
-    __linear_model = None
+    __model = None
     # __column_names: Index = None
 
     def __post_init__(self):
         # self.__column_names = self.data_frame.columns
         self.__X = self.data_frame.drop(self.target, axis=1).values
         self.__y = self.data_frame[self.target].values
-
-    def linear_model(self, test_size):
         self.__X_train, self.__X_test, self.__y_train, self.__y_test = train_test_split(self.__X,
                                                                                         self.__y,
-                                                                                        test_size=test_size,
+                                                                                        test_size=self.test_size,
                                                                                         random_state=42)
-        self.__linear_model = LinearRegression()
+
+    def linear_model(self):
+        self.__model = LinearRegression()
+        return self
+
+    def lasso_model(self):
+        self.__model = Lasso(alpha=self.alpha)
+        return self
+
+    def ridge_model(self):
+        self.__model = Ridge(alpha=self.alpha)
 
     def train(self):
-        self.__linear_model.fit(self.__X_train, self.__y_train)
+        self.__model.fit(self.__X_train, self.__y_train)
+        return self.__X_test, self.__y_test, self.__model
 
-    def simple_predictions(self):
-        return self.__linear_model.predict(self.__X_test)
+
 
 
 """
