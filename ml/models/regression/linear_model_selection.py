9724083157c86e97248f3352dbdc1df8d5ac93eb
Bertan ULUSOY <bertan_ulusoy@yahoo.com>
2023-06-11 21:49:06 +0300

    Cross-validation added to linear model selection.

--- ml/models/regression/linear_model_selection.py
+++ ml/models/regression/linear_model_selection.py
@@ -13,19 +13,24 @@ from sklearn.linear_model import LinearRegression, Lasso, Ridge
 @dataclass()
 class LinearModelSelection(ModelSelection):
     alpha: int = None
+    __kf = None
 
     def __post_init__(self):
-        # self.__column_names = self.data_frame.columns
         self.__X = self.data_frame.drop(self.target, axis=1).values
         self.__y = self.data_frame[self.target].values
         self.__X_train, self.__X_test, self.__y_train, self.__y_test = train_test_split(self.__X,
                                                                                         self.__y,
                                                                                         test_size=self.test_size,
-                                                                                        random_state=42)
+                                                                                        random_state=self.random_state)
+        self.__kf = KFold(n_splits=6, shuffle=True, random_state=42)
 
     def linear_model(self):
         self.__model = LinearRegression()
         return self
+
+    def cross_validation_score(self):
+        """Cross-validation for R-squared"""
+        return cross_val_score(self.__model, self.__X, self.__y, cv=self.__kf)
 
     def lasso_model(self):
         self.__model = Lasso(alpha=self.alpha)
